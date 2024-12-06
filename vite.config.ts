13aafeaa7980b2aef776d2ea273de1d875b22c03
David Enke <david@kvlm.de>
2024-12-06 07:41:25 +0100

    build: narrow browser targets

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import checker from 'vite-plugin-checker';
 import mkcert from 'vite-plugin-mkcert';
 
 // Vite configuration
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig(async () => ({
   // ensure relative asset paths in index.html file
   base: './',
+  // browser targets
+  // https://vite.dev/config/build-options.html#build-target
+  build: { target: ['es2020', 'edge120', 'firefox117', 'chrome120', 'safari17'] },
   plugins: [
     // add type check directly to vite
     checker({ typescript: true, overlay: false }),
